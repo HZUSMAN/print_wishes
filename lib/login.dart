3d1e8247ae8bf2bd59af120550fc47be67963e44
Hafiz Muhammad Usman <107913947+HZUSMAN@users.noreply.github.com>
2022-08-24 06:56:00 -0700

    file update

--- lib/login.dart
+++ lib/login.dart
@@ -13,6 +13,11 @@ class MyLogin extends StatelessWidget{
     return Scaffold(
       body: SingleChildScrollView(
         child: Container(
+
+
+
+
+
           decoration: const BoxDecoration(
               image: DecorationImage(
                   image: AssetImage('assets/login.png'), fit: BoxFit.cover)),
